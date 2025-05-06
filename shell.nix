e7f5aa4ac0221c3c15cc85ca941e1099f2c72b60
WeissNurDerHopper <werknowstoes@gmail.com>
2025-05-06 16:59:04 +0200

    kä

--- shell.nix
+++ shell.nix
@@ -1,8 +1,11 @@
 { pkgs ? import <nixpkgs> { }}: 
 pkgs.mkShell {
-  inputsFrom = [ (pkgs.callPackage ./default.nix { }) ];
+  #inputsFrom = [ (pkgs.callPackage ./default.nix { }) ];
   buildInputs = with pkgs; [
     nodejs
     htmlhint
   ];
+  shellHook = ''
+    nvim index.html my_opinions/music/index.html my_opinions/tv_things/index.html homepage.css my_opinions/music/musicpage.css
+  '';
 }
